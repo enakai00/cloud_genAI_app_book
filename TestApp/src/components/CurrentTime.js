cdc591abd57ac0822fba31fb8740ad9d1a761f8f
Etsuji Nakai <enakai0@gmail.com>
2023-10-27 08:05:23 +0000

    Update

--- TestApp/src/components/CurrentTime.js
+++ TestApp/src/components/CurrentTime.js
@@ -4,13 +4,14 @@ export default function CurrentTime() {
   const [time, setTime] = useState("");
 
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       const now = new Date();
       const hour = now.getHours().toString().padStart(2, "0");
       const min = now.getMinutes().toString().padStart(2, "0");
       const sec = now.getSeconds().toString().padStart(2, "0");
       setTime(hour + ":" + min + ":" + sec);
     }, 1000);
+    return () => { clearInterval(timer) };
   }, []);
 
   const element = (
