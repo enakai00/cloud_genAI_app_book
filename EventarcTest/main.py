d6aa326638d55a644cf13efc2805707520d79cf0
Etsuji Nakai <enakai0@gmail.com>
2023-11-22 06:14:29 +0000

    Update

--- EventarcTest/main.py
+++ EventarcTest/main.py
@@ -3,20 +3,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
 # This handler is triggered by storage events
 @app.route('/api/post', methods=['POST'])
 def process_event():
     event = from_http(request.headers, request.data)
-
-    print('Event contents: {}'.format(event))
-
     event_id = event['id']
+    event_type = event['type']
     bucket_name = event.data['bucket']
     filepath = event.data['name']
     content_type = event.data['contentType']
 
+    print('Event contents: {}'.format(event))
     print('Event ID: {}'.format(event_id))
+    print('Event type: {}'.format(event_type))
     print('Butcket name: {}'.format(bucket_name))
     print('Filepath: {}'.format(filepath))
     print('Content type: {}'.format(content_type))
